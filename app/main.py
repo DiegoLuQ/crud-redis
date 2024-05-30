3ec81c2f2e0f10faf6e259f06338342c0ca9970b
Diego <juan.luque@colegiomacaya.cl>
2024-05-30 11:02:52 -0400

    agregando .env 2

--- app/main.py
+++ app/main.py
@@ -29,5 +29,5 @@ async def delete_data(data:str):
     return True
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8081)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8081)
